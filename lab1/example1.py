96d7b5a84bb1d344893554c2de01799374642c60
e93590105 <berfinyucak@gmail.com>
2020-10-19 09:20:30 +0000

    roots

--- lab1/example1.py
+++ lab1/example1.py
@@ -10,3 +10,11 @@ y = 4
 z = 0.25
 expression = (((2*x+y)**2)*(z**0.5))/(x**0.5 + y**0.5)
 print(expression)
+
+a = 2
+b = 6
+c = -20
+d = (b**2) - (4*a*c)
+root1 = (-b-d**0.5)/(2*a)
+root2 = (-b+d**0.5)/(2*a)
+print(root1 , "," , root2)
