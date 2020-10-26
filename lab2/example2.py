233217084f6723b4ac3dfd9be15bf2048bcda780
e93590105 <berfinyucak@gmail.com>
2020-10-26 08:59:37 +0000

    GPA example

---
+++ lab2/example2.py
@@ -0,0 +1,16 @@
+number1 = float(input("Please input the first number:"))
+number2 = float(input("Please input the second number:"))
+number3 = float(input("Please input the third number:"))
+
+if number1 < number2:
+  if number1 < number3:
+    print("The number which has the minimum value above these three numbers is" , number1)
+  else:
+      print("The number which has the minimum value above these three numbers is" , number3)
+else:
+  if number1 < number3:
+    print("The number which has the minimum value above these three numbers is" , number2)
+    else:
+      print("The number which has the minimum value above these three numbers is" , number3)
+
+
---
+++ lab2/example3.py
@@ -0,0 +1,13 @@
+gpa  = float(input("Please input your GPA:"))
+number_of_lectures  = float(input("Please input the number of lectures you have:"))
+
+if gpa < 2.0:
+  if number_of_lectures < 47:
+    print("Not enough number of lectures and GPA!")
+  else:
+    print("Not enough GPA!")
+else:
+  if number_of_lectures < 47:
+    print("Not enough numbers of lectures!")
+  else:
+    print("GRADUATED!")
